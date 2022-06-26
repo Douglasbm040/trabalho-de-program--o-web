02b731bb91275cd01bf4748c728c96d04bf5a48a
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2022-06-26 17:06:00 -0300

    backend

--- src/scripts/core/CoreRequest.js
+++ src/scripts/core/CoreRequest.js
@@ -6,13 +6,29 @@ export function get(url) {
   return request.responseText;
 }
 
-export function catalog() {
+export function catalogpopularmovie() {
   let data = get(
     "https://api.themoviedb.org/3/movie/popular?api_key=efc27e063cff19aefee686a5f6062149"
   );
   data = JSON.parse(data);
   return data;
-  //console.log(data['results'][0]["title"])
+}
+
+export function catalogpopulartv() {
+  let datatv = get(
+    "https://api.themoviedb.org/3/tv/popular?api_key=efc27e063cff19aefee686a5f6062149&language=en-US&page=1"
+  );
+  datatv = JSON.parse(datatv);
+  return datatv;
+
+}
+
+export function getserie(idtv) {
+  let dataserie = get(
+    "https://api.themoviedb.org/3/tv/"+idtv+"?api_key=efc27e063cff19aefee686a5f6062149&language=en-US&page=1"
+  );
+  dataserie = JSON.parse(dataserie);
+  return dataserie;
 }
 
 export function seach(movie) {
